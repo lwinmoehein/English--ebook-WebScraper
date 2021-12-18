e6786b5c8f9fdbdfb63247b2ae98220f6b9b661e
lwinmoehein <dev.lwinmoehein@gmail.com>
2021-12-19 01:22:13 +0630

    first download success

---
+++ downloadEbooks.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+page = requests.get("https://english-e-reader.net/level/intermediate-plus")
+
+soup = BeautifulSoup(page.content, 'html.parser')
+for a in soup.find_all('a', href=True):
+    if("/book" in a['href']):
+        print(a['href'])
+        driver.get("https://english-e-reader.net"+a['href'])
+        
+        element = driver.find_element_by_class_name('cc-message')
+        element = driver.find_element_by_class_name('cc-window')
+
+        driver.execute_script("""
+        var element = arguments[0];
+        element.parentNode.removeChild(element);
+        """, element)
+
+        
+        driver.find_element(By.ID,"download").click()
+driver.close()
+        
